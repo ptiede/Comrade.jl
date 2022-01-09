cba005d3a9b49c956aca2ff2c81215bb8d7adaf0
Paul Tiede <ptiede91@gmail.com>
2022-01-08 19:35:09 -0500

    added conda

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 if lowercase(get(ENV, "CI", "false")) == "true"
     include("install_pycall.jl")
     using PyCall
-    Conda.pip_interop(true)
+    PyCall.Conda.pip_interop(true)
     PyCall.Conda.pip("install", "ehtim")
 end
 
