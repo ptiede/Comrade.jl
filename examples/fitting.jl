d3796cfbbfa0b93fc846abbdaefcd4367a814245
Paul Tiede <ptiede91@gmail.com>
2022-01-27 16:33:35 -0500

    updated examples

--- examples/fitting.jl
+++ examples/fitting.jl
@@ -11,57 +11,59 @@ using HypercubeTransform
 using Optim
 using Plots
 using ForwardDiff
+using Pathfinder
+using SpeedMapping
+using GCMAES
+using Parameters
+using Nonconvex
+using NLopt
+
 
 
 load_ehtim()
-obs = ehtim.obsdata.load_uvfits("/home/ptiede/Research/ngEHT/ngEHTChallenge2/data/Challenge_2/synthetic_data/M87_GRMHD/eht2022/230/M87_GRMHD_230_122_eht2022_04_allnoise_10s_230GHz_seed-1_netcal.uvfits")
-obs.add_scans()
+obs = ehtim.obsdata.load_uvfits("/run/media/ptiede/Samsung_T5/Research/Projects/JuKeBOX/Observations/eht2022/ma+0.5_r10_n1_tavg.uvfits")
 obsavg = obs.avg_coherent(0.0, scan_avg=true)
-obsavg.add_fractional_noise(0.02)
-dlcamp = extract_lcamp(obsavg; count="min")
-dcphase = extract_cphase(obsavg; count="min-cut0bl")
+obsnn = obsavg.add_fractional_noise(0.03)
 
-lklhd = RadioLikelihood(dlcamp, dcphase)
+damp = extract_amp(obsnn; debias=true)
+dcphase = extract_cphase(obsnn; count="min-cut0bl")
 
-function make_model_mring(θ; nx=6, ny=6)
-    (;r, αamp, αpha, σ, f, rg, τg, ξg, xg, yg) = θ
+
+function make_model_mring(θ)
+    @unpack r, αamp, αpha, σ, f, rg, τg, ξg, xg, yg = θ
     α = αamp.*cos.(αpha)
     β = αamp.*sin.(αpha)
     xg = rg/sqrt(1-τg)
     yg = rg*sqrt(1-τg)
-    #m = RImage(reshape(coeff, 6,6), SqExpPulse(ϵ))
+    #m = DImage(reshape(coeff, 6,6), SqExpPulse(ϵ))
     mg = shifted(rotated(stretched(Gaussian(), xg,yg), ξg), xg, yg)
-    m = convolved(stretched(MRing{length(αpha)}(α, β), r, r), stretched(Gaussian(), σ, σ))
+    m = convolved(stretched(MRing(α, β), r, r), stretched(Gaussian(), σ, σ))
     return m*f + mg*(1-f)
 end
 
 prior_mring = (
          r = Dists.Uniform(μas2rad(5.0), μas2rad(40.0)),
          σ = Dists.Uniform(μas2rad(1.0), μas2rad(40.0)),
-         αamp = For(1:5) do _ Dists.Uniform(0.0, 0.5) end,
-         αpha = For(1:5) do _ Dists.Uniform(-1π, 1π) end,
+         αamp = (Dists.Uniform(0.0, 0.5), Dists.Uniform(0.0, 0.5)),
+         αpha = (Dists.Uniform(-π, π), Dists.Uniform(-π, π)),
          rg = Dists.Uniform(μas2rad(20.0), μas2rad(200.0)),
-         xg = Dists.Uniform(μas2rad(-500.0), μas2rad(500.0)),
-         yg = Dists.Uniform(μas2rad(-500.0), μas2rad(500.0)),
+         xg = Dists.Uniform(μas2rad(-50.0), μas2rad(50.0)),
+         yg = Dists.Uniform(μas2rad(-50.0), μas2rad(50.0)),
          τg = Dists.Uniform(0.0, 0.9),
          ξg = Dists.Uniform(-π/2, π/2),
          f = Dists.Uniform(0.0, 1.0)
-
         )
 
 
-function make_model_rimg(θ; r=μas2rad(50.0), nx=6, ny=6)
-    (;ϵ, coeff, f, rg, τg, ξg, xg, yg) = θ
-    m = stretched(RImage(reshape(coeff, ny,nx), SqExpPulse(ϵ)), r, r)
-    xg = rg/sqrt(1-τg)
-    yg = rg*sqrt(1-τg)
-    mg = shifted(rotated(stretched(Gaussian(), xg,yg), ξg), xg, yg)
-    return m*f + mg*(1-f)
+function make_model_rimg(θ; r=μas2rad(60.0), nx=6, ny=6)
+    @unpack coeff, f = θ
+    m = stretched(DImage(reshape(coeff, ny,nx), BSplinePulse{3}()), r, r)
+    return m*f
 end
 
-function make_model_trimg(θ; rimg=μas2rad(80.0), nx=8, ny=8)
-    (;ϵ, coeff, r, αamp, αpha, σ, f, x0, y0) = θ
-    m = shifted(stretched(RImage(reshape(coeff, ny,nx), SqExpPulse(ϵ)), rimg, rimg), x0, y0)
+function make_model_trimg(θ; rimg=μas2rad(85.0), nx=7, ny=7)
+    @unpack coeff, r, αamp, αpha, σ, f = θ
+    m = stretched(DImage(reshape(coeff, ny,nx), BSplinePulse{3}()), rimg, rimg)
     α = αamp.*cos.(αpha)
     β = αamp.*sin.(αpha)
     #mr = MRing(α, β)
@@ -71,33 +73,18 @@ end
 
 nx = ny = 8
 prior_trimg = (
-         ϵ = Dists.Uniform(0.2, 4.0),
          coeff = Dists.Dirichlet(nx*ny, 1.0),
          r = Dists.Uniform(μas2rad(5.0), μas2rad(40.0)),
-         σ = Dists.Uniform(μas2rad(0.1), μas2rad(1.0)),
+         σ = Dists.Uniform(μas2rad(0.1), μas2rad(5.0)),
          αamp = ntuple(x->Dists.Uniform(0.0 ,0.5), 2),
          αpha = ntuple(x->Dists.Uniform(-π, π), 2),
          f = Dists.Uniform(0.0, 1.0),
-         x0 = Dists.Uniform(-μas2rad(40.0), μas2rad(40.0)),
-         y0 = Dists.Uniform(-μas2rad(40.0), μas2rad(40.0))
         )
 prior_rimg = (
-            ϵ = Dists.Uniform(1.0/3, 3.0),
             coeff = Dists.Dirichlet(nx*ny, 1.0),
-            rg = Dists.Uniform(μas2rad(200.0), μas2rad(2000.0)),
-            xg = Dists.Uniform(μas2rad(-500.0), μas2rad(500.0)),
-            yg = Dists.Uniform(μas2rad(-500.0), μas2rad(500.0)),
-            τg = Dists.Uniform(0.0, 0.9),
-            ξg = Dists.Uniform(-π/2, π/2),
-            f = Dists.Uniform(0.0, 1.0)
-
+            f = Dists.Uniform(0.0 ,3.0)
            )
 
-
-tr = asflat(prior_trimg)
-tt = asflat(prior_rimg)
-tc = ascube(prior_mring)
-nd = dimension(tr)
 
 function make_lpost(lklhd, prior, make_model)
     tr = asflat(prior)
@@ -108,27 +95,58 @@ function make_lpost(lklhd, prior, make_model)
     end
 end
 
-lp = make_lpost(lklhd, prior_trimg, make_model_trimg)
+function make_grad(lp, chunk)
+    nd = dimension(lp.tr)
+    out = zeros(nd)
+    cfg = ForwardDiff.GradientConfig(lp, rand(nd), ForwardDiff.Chunk{chunk}())
+    x->ForwardDiff.gradient!(out, lp, x, cfg)
+end
+
+
+lklhd = RadioLikelihood(damp, dcphase)
+
+lp = make_lpost(lklhd, prior_mring, make_model_mring)
+nd = dimension(lp.tr)
 lp(rand(nd))
-∇lpost(x) = ForwardDiff.gradient(lpost, x)
-∇lpost(rand(nd))
-@time ∇lpost(rand(nd));
-f = OptimizationFunction((x,p)->-lpost(x), GalacticOptim.AutoForwardDiff())
-res = bboptimize(x->-lpost(x);
-                 SearchRange=(-7.0, 7.0),
-                 NumDimensions=nd,
-                 MaxFuncEvals=50_000
-                )
-@show best_fitness(res)
-prob = GalacticOptim.OptimizationProblem(f, best_candidate(res)+randn(nd)*0.5, nothing)
-prob = GalacticOptim.OptimizationProblem(f, sol.u+randn(nd)*0.5, nothing)
-sol = solve(prob, LBFGS())
-@show sol.minimum
-xopt = transform(tr, sol.u)
 
+∂lp = make_grad(lp, 2)
+∂lp(rand(nd))
+@benchmark ∂lp($rand(nd))
 
-mopt = make_model_trimg(xopt; nx, ny)
-img = intensitymap(mopt, μas2rad(200.0), μas2rad(200.0), 1024, 1024)
+using Pathfinder
+θ₀s = collect.(eachcol(rand(nd, 50) .* 5 .- 2.5))
+res = map(x->pathfinder(lp, ∂lp, rand(nd)*8.0, 10), 1:50);
+
+xopts = transform.(Ref(lp.tr), eachcol(res[6][2]))
+
+# fopt = OptimizationFunction((x,p)->-lp(x), GalacticOptim.AutoForwardDiff{25}())
+# prob = GalacticOptim.OptimizationProblem(fopt, 2*randn(nd), nothing, lb = fill(-5.0, nd), ub = fill(5.0, nd))
+# sol1 = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited(), maxiter=200, maxtime=500.0)
+# @info "First min" sol1.minimum
+
+# prob = GalacticOptim.OptimizationProblem(fopt, 3*randn(nd), nothing, lb = fill(-10.0, nd), ub = fill(10.0, nd))
+# sol = solve(prob, LBFGS())
+# @show sol.minimum
+# xopt = transform(lp.tr, sol1.u)
+
+mopt = make_model_rimg(xopts[1]; nx, ny)
+img = intensitymap(mopt, μas2rad(100.0), μas2rad(100.0), 1024, 1024)
 
 x,y = imagepixels(img)
 heatmap(rad2μas(x), rad2μas(y), img, xflip=true, aspect_ratio=:equal, size=(600,500))
+
+metric = DiagEuclideanMetric(nd)
+hamiltonian = Hamiltonian(metric, lp, x->(lp(x),∂lp(x)))
+ϵ0 = find_good_stepsize(hamiltonian, ϕ[:,1])
+integrator = Leapfrog(ϵ0)
+
+proposal = NUTS{MultinomialTS, GeneralisedNoUTurn}(integrator)
+adaptor = StanHMCAdaptor(MassMatrixAdaptor(metric), StepSizeAdaptor(0.8, integrator))
+
+samples, stats = sample(hamiltonian, proposal, ϕ[:,1], 2000, adaptor, 1000; progress=true)
+
+
+ac = arrayconfig(damp)
+u,v = getuv(ac)
+scatter(hypot.(u,v), (amplitude.(damp.data)))
+scatter!(hypot.(u,v), amplitude.(Ref(mopt), u, v))
--- examples/jukebox.jl
+++ examples/jukebox.jl
@@ -14,6 +14,7 @@ using JuKeBOX
 const Jk = JuKeBOX
 using BlackBoxOptim
 using GCMAES
+using Parameters
 
 
 load_ehtim()
@@ -29,36 +30,43 @@ lklhd = RadioLikelihood(dlcamp, dcphase)
 
 
 function make_model(θ)
-    (;spin, inc, rpeak, width, β, χ, ι, θg, ξ) = θ
+    @unpack spin, inc, a, b, β, ξ = θ
+    θg = μas2rad(3.364)
+    r0 = 4.5
+    spin = -0.94
+    inc = deg2rad(17.0)
+    χ = deg2rad(-135.0)
+    ι = π/3
+    g = Jk.Kerr(spin)
     #(;spin, inc, θg, ξ) = θ
+    prf = Jk.GaussianRing(r0, a, b)
+    v = Jk.FluidVelocity(β, χ)
+    bmag = Jk.MagneticField(ι, χ+1π)
+    T = typeof(spin)
     o = Jk.Observer(1, inc)
-    g, acc = Jk.bam(1, spin, 1.0, rpeak, width, β, χ, ι)
+    acc = BAM(1, 1.0, prf, v, bmag)
     macc = SingleStokes(Jk.SimpleModel(acc, g, o), :I)
-    mimg = modelimage(macc; fovx=30.0, fovy=30.0, nx=64, ny=64)
-    #fm = flux(mimg)
-    return rotated(stretched(mimg, θg, θg), ξ)
+    mimg = modelimage(macc; fovx=40.0, fovy=40.0, nx=128, ny=128)
+    fm = flux(mimg)
+    return rotated(stretched(mimg, θg, θg), ξ)/fm
 end
 
-function test()
-    params = transform(tf, rand(dimension(tf)))
-    model = make_model(params)
-    #heatmap(intensitymap(model, 150.0, 150.0, 256, 256))
-end
 
 prior = (
-         spin = Dists.Uniform(-0.98, 0.98),
-         inc = Dists.Uniform(0.01, π/2),
-         rpeak = Dists.Uniform(2.1, 10.0),
-         width = Dists.Uniform(1.0, 10.0),
+         spin = Dists.Uniform(-0.98, 0),
+         r0 = Dists.Uniform(2.0, 8.0),
+         inc = Dists.Uniform(deg2rad(5.0), deg2rad(40.0)),
+         a = Dists.Uniform(-4.0, 4.0),
+         b = Dists.Uniform(1.0, 5.0),
          β = Dists.Uniform(0.0, 0.98),
-         χ = Dists.Uniform(0.0, π/2),
-         ι = Dists.Uniform(0.0, π/2),
+         χ = Dists.Uniform(0.0, π),
+         ι = Dists.Uniform(0.0, π),
          θg = Dists.Uniform( μas2rad(0.1), μas2rad(10.0)),
          ξ = Dists.Uniform(-π, π),
          #f = Dists.Uniform(0.0, 1.0)
         )
 
-function make_lpost(lklhd, prior, make_model)
+function make_lpost(lklhd, prior, make_model; chunk=5)
     tr = asflat(prior)
     function lpost(x)
         y, logjac = transform_and_logjac(tr, x)
@@ -68,20 +76,16 @@ function make_lpost(lklhd, prior, make_model)
 end
 
 
-lp = make_lpost(lklhd, prior, make_model)
+lp = make_lpost(lklhd, prior, make_model);
 nd = dimension(lp.tr)
-p = make_model(transform(lp.tr, rand(nd)))
-lp(rand(nd))
-@btime lp($rand(nd))
-∇lpost(x) = ForwardDiff.gradient(lp, x)
-∇lpost(rand(nd))
-@time ∇lpost(rand(nd));
-f = OptimizationFunction((x,p)->-lp(x))
-prob = GalacticOptim.OptimizationProblem(f, rand(nd), nothing, lb = fill(0.01, nd), ub = fill(0.99, nd))
-sol = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited(), maxiters=10_000, maxtime=1000.0)
-f = OptimizationFunction((x,p)->-lp(x), GalacticOptim.AutoForwardDiff())
-prob = GalacticOptim.OptimizationProblem(f, sol.u, nothing, lb = fill(0.01, nd), ub = fill(0.99, nd))
-sol = solve(prob, GCMAESOpt())
+@time lp(rand(nd))
+
+fopt = OptimizationFunction((x,p)->-lp(x), GalacticOptim.AutoForwardDiff{5}())
+prob = GalacticOptim.OptimizationProblem(fopt, rand(nd), nothing, lb = fill(-5.0, nd), ub = fill(5.0, nd))
+sol = solve(prob, BBO_adaptive_de_rand_1_bin_radiuslimited(), maxiters=200, maxtime=500.0)
+@info "First min" sol.minimum
+prob = GalacticOptim.OptimizationProblem(fopt, sol.u, nothing, lb = fill(-10.0, nd), ub = fill(10.0, nd))
+sol = solve(prob, GCMAESOpt(); maxiters=500)
 @show sol.minimum
 xopt = transform(lp.tr, sol.u)
 
@@ -91,8 +95,11 @@ dvis = extract_vis(obsavg)
 ac = arrayconfig(dvis)
 u,v = getuv(ac)
 scatter(hypot.(u,v), abs.(visibility.(dvis.data)))
-scatter!(hypot.(u,v), amplitude.(Ref(mopt), u, v))
-img = intensitymap(mopt, μas2rad(100.0), μas2rad(100.0), 64, 64)
+scatter!(hypot.(u,v), amplitude.(Ref(mopt), u, v)./4.0)
+img = intensitymap(mopt, μas2rad(100.0), μas2rad(100.0), 128, 128)
 
 x,y = imagepixels(img)
 heatmap(rad2μas(x), rad2μas(y), img, xflip=true, aspect_ratio=:equal, size=(600,500))
+savefig(joinpath(@__DIR__,"m87_bestfit.png"))
+
+scatter(abs2.((ll.x .-  ll.k.ops.μ(mopt/20.0)).*ll.k.ops.κ(2)))
