0b92a4d7051942873756a0afae09f8363b9db4a7
Paul Tiede <ptiede91@gmail.com>
2022-02-04 10:18:32 -0500

    start nfft addition

--- src/models/nfft.jl
+++ src/models/nfft.jl
@@ -1,19 +1,24 @@
-function create_cache(alg::NFFT, img)
-    dx,dy = pixelsizes(img)
-    uv = Matrix{eltype(u)}(undef, 2, length(u))
-    uv[1,:] = u*dx
-    uv[2,:] = v*dy
-    dx,dy = pixelsizes(img)
+function padimage(img, alg::NFFT)
+    padfac = alg.padfac
     ny,nx = size(img)
     nnx = nextpow(2, padfac*nx)
     nny = nextpow(2, padfac*ny)
     nsx = nnx÷2-nx÷2
     nsy = nny÷2-ny÷2
-    pimg = PaddedView(zero(eltype(img)), img.im,
+    return PaddedView(zero(eltype(img)), img.im,
                       (1:nnx, 1:nny),
                       (nsx+1:nsx+nx, nsy+1:nsy+ny)
                      )
-    plan = plan_nfft(uv, size(pimg), m)
+end
+
+function create_cache(alg::NFFT, img)
+    pimg = padimage(img, alg)
+
+    # No supplied uv positions so can't plan NFFT
+    isnothing(alg.uv) && return NFFTCache(alg, nothing, LinearAlgebra.I, pimg)
+
+    # User supplied
+    plan = plan_nfft(alg.uv, size(pimg), alg.m)
     phases = @. exp(1im*π*(u*dx + v*dy))*dx*dy#*exp(1im*π*(u*x + v*y))*dx*dy
     return plan, phases, pimg
 end
@@ -21,10 +26,12 @@ end
 Base.@kwdef struct NFFT <: FourierTransform
     padfac::Int = 2
     m::Int = 10
+    uv::T = nothing
 end
 
-struct NFFTCache{C} <: AbstractCache
-    plan::C
-    padfac::Int
-    m::Int
+struct NFFTCache{A<:NFFT,C,M,I} <: AbstractCache
+    alg::A
+    plan::P
+    phases::M
+    pimg::I
 end
