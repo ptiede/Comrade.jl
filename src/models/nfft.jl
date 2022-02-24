f84cb8f3aee8d6597a3a5d93c29059b87683c440
Paul Tiede <ptiede91@gmail.com>
2022-02-24 11:25:53 -0500

    update nfft

--- src/models/nfft.jl
+++ src/models/nfft.jl
@@ -1,7 +1,7 @@
 export NFFTAlg
 
 Base.@kwdef struct NFFTAlg{T} <: FourierTransform
-    padfac::Int = 2
+    padfac::Int = 1
     m::Int = 10
     uv::T = nothing
 end
@@ -11,6 +11,11 @@ function NFFTAlg(u::AbstractArray, v::AbstractArray; padfac=2, m=10)
     uv[1,:] .= u
     uv[2,:] .= v
     return NFFTAlg(;padfac, m, uv)
+end
+
+function NFFTAlg(ac::ArrayConfiguration; padfac=2, m=10)
+    u, v = getuv(ac)
+    return NFFTAlg(u, v, padfac, m=10)
 end
 
 struct NFFTCache{A<:NFFTAlg,P,M,I} <: AbstractCache
@@ -41,8 +46,11 @@ end
     isnothing(alg.uv) && return NFFTCache(alg, nothing, LinearAlgebra.I, pimg)
     dx, dy = pixelsizes(img)
     # User supplied
-    plan = plan_nfft(uv2, size(pimg))
-    phases = @. cispi((@view(uv[1,:])*dx + @view(uv[2,:])*dy))*dx*dy#*exp(1im*π*(u*x + v*y))*dx*dy
+    uv2 = copy(alg.uv)
+    uv2[1,:] .= uv2[1,:]*dx
+    uv2[2,:] .= uv2[2,:]*dx
+    plan = plan_nfft(uv2, size(pimg); precompute=NFFT.POLYNOMIAL)
+    phases = @. cispi((@view(uv2[1,:]) + @view(uv2[2,:])))*dx*dy
     return NFFTCache(alg, plan, phases, pimg)
 end
 
@@ -53,20 +61,16 @@ function phasenfft!(vis, phases)
     return vis
 end
 
-function nocachevis(m::ModelImage, u, v)
-    img = m.cache.pimg
+function visibilities(cache::NFFTCache)
+    vis = cache.plan*cache.pimg'
+    return phasenfft!(vis, cache.phases)
+end
 
-    dx,dy = pixelsizes(m.image)
-    uv = zeros(2, length(u))
-    uv[1,:] = u*dx
-    uv[2,:] = v*dy
-    #compute the nfft
-    #convert to complex
-    p = plan_nfft(uv, size(img))
-    vis = p*img
-    # phase center
-    phases = @. cispi((@view(uv[1,:]) + @view(uv[2,:])))*dx*dy
-    phasenfft!(vis, phases)
+function nocachevis(m::ModelImage, u, v)
+    alg = NFFTAlg(u,v; padfac=m.cache.alg.padfac, m=m.cache.alg.m)
+    cache = create_cache(alg, m.image)
+    m = @set m.cache = cache
+    visibilities(m, u, v)
 end
 
 function checkuv(uv, u, v)
@@ -75,7 +79,7 @@ function checkuv(uv, u, v)
 end
 
 
-function _visibilities(m::ModelImage{M,I,C}, u::AbstractArray, v::AbstractArray) where {M,I,C<:NFFTCache}
+function visibilities(m::ModelImage{M,I,C}, u::AbstractArray, v::AbstractArray) where {M,I,C<:NFFTCache}
     if isnothing(m.cache.plan)
         return nocachevis(m, u, v)
     else
@@ -84,4 +88,80 @@ function _visibilities(m::ModelImage{M,I,C}, u::AbstractArray, v::AbstractArray)
         vis .= vis.*m.cache.phases
         return vis
     end
+end
+
+function amplitudes(m::ModelImage{M,I,C}, u::AbstractArray, v::AbstractArray) where {M,I,C<:NFFTCache}
+    if isnothing(m.cache.plan)
+        return nocachevis(m, u, v)
+    else
+        checkuv(m.cache.alg.uv, u, v)
+        vis =  m.cache.plan*m.cache.pimg
+        vis .= abs.(vis)
+        return vis
+    end
+end
+
+function _recache(m)
+    c = NFFTCache(m.cache.alg, nothing, nothing, m.cache.pimg)
+    m2 = ModelImage(m.model, m.image, c)
+    return m2
+end
+
+function bispectra(m::ModelImage{M,I,C},
+                    u1::AbstractArray,
+                    v1::AbstractArray,
+                    u2::AbstractArray,
+                    v2::AbstractArray,
+                    u3::AbstractArray,
+                    v3::AbstractArray) where {M,I,C<:NFFTCache}
+    # TODO: Fix this so we can actually use a stored cache for closures
+    n = length(u1)
+    # In testing it is faster to concatenate everything into 1 vector and then
+    # take an NFFT of that
+    u = vcat(u1,u2,u3)
+    v = vcat(v1,v2,v3)
+    vis = nocachevis(m, u, v)
+    @inbounds (@view(vis[1:n])).*(@view(vis[n+1:2n])).*(@view vis[2n+1:3n])
+end
+
+function closure_phases(m::ModelImage{M,I,C},
+                        u1::AbstractArray,
+                        v1::AbstractArray,
+                        u2::AbstractArray,
+                        v2::AbstractArray,
+                        u3::AbstractArray,
+                        v3::AbstractArray) where {M,I,C<:NFFTCache}
+    # TODO: Fix this so we can actually use a stored cache for closures
+    n = length(u1)
+    # In testing it is faster to concatenate everything into 1 vector and then
+    # take an NFFT of that
+    u = vcat(u1,u2,u3)
+    v = vcat(v1,v2,v3)
+    vis = nocachevis(m, u, v)
+    phase = @inbounds angle.(@view(vis[1:n])) .+ angle.(@view(vis[n+1:2n])) .+ angle.(@view vis[2n+1:3n])
+    return phase
+end
+
+function logclosure_amplitudes(m::ModelImage{M,I,C},
+                               u1::AbstractArray,
+                               v1::AbstractArray,
+                               u2::AbstractArray,
+                               v2::AbstractArray,
+                               u3::AbstractArray,
+                               v3::AbstractArray,
+                               u4::AbstractArray,
+                               v4::AbstractArray,
+                               ) where {M,I,C<:NFFTCache}
+    # TODO: Fix this so we can actually use a stored cache for closures
+    n = length(u1)
+    u = vcat(u1,u2,u3,u4)
+    v = vcat(v1,v2,v3,v4)
+    vis = nocachevis(m, u, v)
+    amp1 = @view vis[1:n]
+    amp2 = @view vis[n+1:2n]
+    amp3 = @view vis[2n+1:3n]
+    amp4 = @view vis[3n+1:4n]
+
+    lcamp = @. log(amp1*amp2/(amp3*amp4))
+    return lcamp
 end
