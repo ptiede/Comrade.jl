13588d077f8dd2eeec760bf46ca57a19b8f624ef
Paul Tiede <ptiede91@gmail.com>
2022-11-21 13:05:01 -0500

    Fix posterior issue from rebase

--- src/distributions/radiolikelihood.jl
+++ src/distributions/radiolikelihood.jl
@@ -13,7 +13,7 @@ such as when fitting different wavelengths or days, you can combine them using
 # Example
 
 ```julia-repl
-julia> RadioLikelihood(dcphase1, dlcamp1)
+julia> RadioLikelihood(model, dcphase1, dlcamp1)
 ```
 """
 struct RadioLikelihood{M,T,A} <: MB.AbstractMeasure
@@ -22,12 +22,65 @@ struct RadioLikelihood{M,T,A} <: MB.AbstractMeasure
     ac::A
 end
 
+struct ModelMetadata{M, C}
+    model::M
+    metadata::C
+end
+
+function (m::ModelMetadata)(θ)
+    return m.model(θ, m.metadata)
+end
+
+
+
 
 function RadioLikelihood(model, data::EHTObservation...)
     ls = Tuple(map(makelikelihood, data))
     acs = arrayconfig.(data)
     #@argcheck acs[1] == acs[2]
     RadioLikelihood{typeof(model), typeof(ls), typeof(acs[1])}(model, ls, acs[1])
+end
+
+"""
+    RadioLikelihood(model, metadata, obs::EHTObservation...)
+
+Creates a RadioLikelihood using the `model` and its related `metadata`. The `model`
+is a function that converts from parameters `θ` to a Comrade
+AbstractModel which can be used to compute [`visibilities`](@ref) and a set of
+`metadata` that is used by `model` to compute the model.
+
+# Warning
+
+The `model` itself must be a two argument function where the first argument is the set
+of model parameters and the second is a container that holds all the additional
+information needed to construct the model. An example of this is when the model
+needs some precomputed cache to define the model.
+
+# Example
+```julia
+
+cache = create_cache(FFTAlg(), IntensityMap(zeros(128,128), μas2rad(100.0), μas2rad(100.0)))
+
+function model(θ, metadata)
+    (; r, a) = θ
+    m = stretched(ExtendedRing(a), r, r)
+    return modelimage(m, metadata.cache)
+end
+
+prior = (
+         r = Uniform(μas2rad(10.0), μas2rad(40.0)),
+         a = Uniform(0.1, 5.0)
+         )
+
+RadioLikelihood(model, (cache = cache), obs)
+```
+"""
+function RadioLikelihood(model, metadata::NamedTuple, data::EHTObservation...)
+    ls = Tuple(map(makelikelihood, data))
+    acs = arrayconfig.(data)
+    #@argcheck acs[1] == acs[2]
+    mms = ModelMetadata(model, metadata)
+    RadioLikelihood{typeof(model), typeof(ls), typeof(acs[1])}(model, metadata, ls, acs[1])
 end
 
 """
