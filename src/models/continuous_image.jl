c8d48945ca498033e75f46a1db16741ad9a458de
Paul Tiede <ptiede91@gmail.com>
2022-11-28 23:25:25 -0500

    generalize ContinuousImage

--- src/models/continuous_image.jl
+++ src/models/continuous_image.jl
@@ -17,10 +17,18 @@ struct ContinuousImage{A <: KeyedArray, P} <: AbstractModel
     kernel::P
 end
 
+function ContinuousImage(img::IntensityMap, pulse::AbstractModel)
+    dx, dy = pixelsizes(img)
+    spulse = stretched(pulse, dx, dy)
+    return ContinuousImage{typeof(img), typeof(spulse)}(img, spulse)
+end
+
+
 function ContinuousImage(im::AbstractMatrix, fovx::Real, fovy::Real, x0::Real, y0::Real, pulse, header=nothing)
     xitr, yitr = imagepixels(fovx, fovy, size(img, 1), size(img,2), x0, y0)
     img = IntensityMap(im, (X=xitr, Y=yitr), header)
-    return ContinuousImage(img, pulse)
+    spulse = stretched(pulse, step(xitr), step(yitr))
+    return ContinuousImage(img, spulse)
 end
 
 function ContinuousImage(im::AbstractMatrix, fov::Real, x0::Real, y0::Real, pulse, header=nothing)
@@ -38,13 +46,16 @@ visanalytic(::Type{<:ContinuousImage}) = NotAnalytic() # not analytic b/c we wan
 imanalytic(::Type{<:ContinuousImage}) = IsAnalytic()
 isprimitive(::Type{<:ContinuousImage}) = IsPrimitive()
 
+radialextent(c::ContinuousImage) = maximum(values(fieldofview(c.img)))/2
+
 function intensity_point(m::ContinuousImage, p)
-    dx, dy = pixelsizes(m)
     sum = zero(eltype(m.img))
     @inbounds for (I, p0) in pairs(grid(m.img))
-        dp = (p.X - p0.X, p.Y - p0.Y)
-        k = intensity_point(m.pulse, dp)
-        sum += m.img[I]*k/(dx*dy)
+        dp = (X=(p.X - p0.X), Y=(p.Y - p0.Y))
+        k = intensity_point(m.kernel, dp)
+        sum += m.img[I]*k
     end
     return sum
 end
+
+convolved(cimg::ContinuousImage, m::AbstractModel) = ContinuousImage(cimg.img, convolved(cimg.pulse, m))
