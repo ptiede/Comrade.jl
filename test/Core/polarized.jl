fa63f0c4f0788ccface96be8917b411fd5f33472
Paul Tiede <ptiede91@gmail.com>
2023-02-02 10:03:58 -0500

    Add explicit polarization test

--- test/Core/polarized.jl
+++ test/Core/polarized.jl
@@ -55,3 +55,34 @@ end
     s = map(length, dims(g))
     testpol(modelimage(m, IntensityMap(zeros(StokesParams{Float64}, s), g)))
 end
+
+@testset "Rotation" begin
+    m = PolarizedModel(Gaussian(), Gaussian(), ZeroModel(), 0.1*Gaussian())
+    g = GriddedKeys(imagepixels(5.0, 5.0, 128, 128))
+    img1 = intensitymap(m, g)
+    @test all(==(1), stokes(img1, :Q) .≈ stokes(img1, :I))
+    @test all(==(1), stokes(img1, :U) .≈ 0.0*stokes(img1, :I))
+    @test all(==(1), stokes(img1, :V) .≈ 0.1*stokes(img1, :I))
+
+    # Now we rotate into perfect U
+    img2 = intensitymap(rotated(m,  π/4), g)
+    @test all(==(1), stokes(img2, :U) .≈ stokes(img1, :I))
+    @test all(==(1), isapprox.(stokes(img2, :Q), 0.0, atol=1e-16))
+    @test all(==(1), stokes(img1, :V) .≈ stokes(img2, :V))
+
+    # Now we rotate into perfectly -Q
+    img3 = intensitymap(rotated(m,  π/2), g)
+    @test all(==(1), stokes(img3, :Q) .≈ -stokes(img3, :I))
+    @test all(==(1), isapprox.(stokes(img3, :U), 0.0, atol=1e-16))
+    @test all(==(1), stokes(img1, :V) .≈ stokes(img3, :V))
+
+    # Now we rotate into perfectly -U
+    img4 = intensitymap(rotated(m,  3π/4), g)
+    @test all(==(1), stokes(img4, :U) .≈ -stokes(img4, :I))
+    @test all(==(1), isapprox.(stokes(img4, :Q), 0.0, atol=1e-16))
+    @test all(==(1), stokes(img1, :V) .≈ stokes(img4, :V))
+
+    # Now make sure it is π periodic
+    img5 = intensitymap(rotated(m,  -π/4), g)
+    @test all(==(1), img4 .≈ img5)
+end
