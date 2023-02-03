f10edac797f48022f5692158c2080db2ba2c98f5
Paul Tiede <ptiede91@gmail.com>
2023-02-02 22:51:25 -0500

    Start updating closure only imaging

--- examples/imaging_closures.jl
+++ examples/imaging_closures.jl
@@ -1,28 +1,40 @@
-using Pkg; Pkg.activate(@__DIR__)
-#Pkg.add(url="https://github.com/ptiede/RadioImagePriors.jl")
-using Comrade
-using Distributions
-using ComradeOptimization
-using ComradeAHMC
-using OptimizationBBO
-using OptimizationOptimJL
-using Zygote
-using Plots
-using StatsBase
-using RadioImagePriors
+# # Imaging a Black Hole using only Closure Quantities
+# The EHT is the highest resolution telescope ever created. Its resolution is equivalent
+# to roughly tracking a hockey puck on the moon when viewing it from earth. However,
+# the EHT is also a unique interferometer. For one the data is produces is incredible sparse.
+# The telescope it only from 8 geographic locations around the planet, each with its unique
+# telescope. Additionally, the EHT observes at a much higher frequency than typical interferometers.
+# As a result, the EHT data is often poorly calibrated. Meaning there can be large instrumental effects
+# often called *gains* that can corrupt our signal. One way to deal with this is to fit quantities
+# that are independent of gains, these are often called **closure quantities**. There are two
+# type of closure quantities for total intensity observations:
+#  1. Closure phases
+#  2. Log-closure amplitudes
+#  In this tutorial we will explore imaging M87 using just closure quantities. Note that while
+# this simplifies the model, we are throwing out additional information we know about the gains
+# so often the images are of poorer quality than fitting more directly the visibilities measured
+# by the telescope.
 
-# load eht-imaging we use this to load eht data
-load_ehtim()
+using Pkg; Pkg.activate(@__DIR__)
+
+# To get started we will load Comrade
+using Comrade
+
+
 # To download the data visit https://doi.org/10.25739/g85n-f134
-obs = ehtim.obsdata.load_uvfits(joinpath(@__DIR__, "SR1_M87_2017_096_hi_hops_netcal_StokesI.uvfits"))
-obs.add_scans()
-# kill 0-baselines since we don't care about
-# large scale flux and make scan-average data
-obs = scan_average(obs).add_fractional_noise(0.02)
-# extract log closure amplitudes and closure phases
+# To load the eht-imaging obsdata object we do:
+obs = load_ehtim_uvfits(joinpath(@__DIR__, "SR1_M87_2017_096_hi_hops_netcal_StokesI.uvfits"))
+
+# Now we do some minor preprocessing:
+#   - Scan average the data since the data have been preprocessed so that the gain phases
+#      coherent.
+obs = scan_average(obs).add_fractional_noise(0.01)
+
+# For this tutorial we will stick to fitting closure only data, although we can
+# get better results by also modeling gains, since closure only modeling is equivalent
+# to assuming infinite gain priors.
 dlcamp = extract_lcamp(obs)
-dcphase = extract_cphase(obs)
-damp = extract_amp(obs)
+dcphase = extract_cphase(obs; cut_trivial=true)
 
 # Build the Model. Here we we a struct to hold some caches
 # which will speed up imaging
@@ -42,30 +54,29 @@ end
 # For our model we will be using a rasterized image. This can be viewed as something like a
 # non-parametric model. As a result of this we will need to use a `modelimage` object to
 # store cache information we will need to compute the numerical FT.
-function (model::Model)(θ)
+function model(θ, metadata)
     (;c) = θ
-    #Construct the image model
-    img = IntensityMap(c, model.fovx, model.fovy, BSplinePulse{3}())
-    #Create the modelimage object that will use a cache to compute the DFT
-    return modelimage(img, model.cache)
+    (; grid, cache) = metadata
+    # Construct the image model
+    img = IntensityMap(c, grid)
+    return  ContinuousImage(img, cache)
 end
 
 
-npix = 16
+npix = 12
 fovxy = μas2rad(70.0)
 # Now we can feed in the array information to form the cache. We will be using a DFT since
 # it is efficient for so few pixels
-mms = Model(dlcamp, fovx, fovy, nx, ny)
 # We will use a Dirichlet prior to enforce that the flux sums to unity since closures are
 # degenerate to total flux.
-img = IntensityMap(zeros(npix,npix), fovxy, fovxy)
-xitr, yitr = Comrade.imagepixels(img)
-prior = (c = CenteredImage(xitr, yitr, μas2rad(1.0),
-            ImageDirichlet(1.0, npix, npix)
-            )
-            ,)
+grid = imagepixels(fovxy, fovxy, npix, npix)
+buffer = IntensityMap(zeros(npix,npix), grid)
+cache = create_cache(DFTAlg(dlcamp), buffer, BSplinePulse{3}())
+metadata = (;grid, cache)
 
-lklhd = RadioLikelihood(mms, dlcamp, dcphase)
+prior = (c = ImageDirichlet(1.0, npix, npix), )
+
+lklhd = RadioLikelihood(model, metadata, dlcamp, dcphase)
 post = Posterior(lklhd, prior)
 
 # Transform from simplex space to the unconstrained
@@ -75,11 +86,12 @@ tpost = asflat(post)
 # Let's run an optimizer to get a nice starting location
 # It turns out that gradients are really helpful here
 ndim = dimension(tpost)
+using Zygote
 # Creates optimization function using Optimization.jl
 f = OptimizationFunction(tpost, Optimization.AutoZygote())
 # randn(ndim) is a random initialization guess
 # nothing just says there are no additional arguments to the optimization function.
-prob = OptimizationProblem(f, randn(ndim), nothing)
+prob = OptimizationProblem(f, rand(ndim) .- 0.5, nothing)
 
 ℓ = logdensityof(tpost)
 # Find the best fit image! Using LBFGS optimizaer.
@@ -90,19 +102,18 @@ sol = solve(prob, LBFGS(); maxiters=2_000, callback=(x,p)->(@info ℓ(x); false)
 xopt = transform(tpost, sol)
 
 # Let's see how the fit looks
-xopt = transform(tpost, res.draws[:,1])
-img = intensitymap(mms(xopt), fovx, fovy, 512, 512)
-plot(img, colorbar_scale=:log10, size=(450,600), clims=(1e-6, 1e-3))
-residual(mms(xopt), dlcamp)
-residual(mms(xopt), dcphase)
+img = intensitymap(model(xopt, metadata), fovxy, fovxy, 512, 512)
+plot(img)
+residual(model(xopt, metadata), dlcamp)
+residual(model(xopt, metadata), dcphase)
 
 # Finally we sample with HMC
-metric = DenseEuclideanMetric(Matrix(res.fit_distribution.Σ))
+metric = DiagEuclideanMetric(ndim)
 hchain, stats = sample(post, AHMC(;metric, autodiff=AD.ZygoteBackend()), 5000; nadapts=4000, init_params=xopt)
 
 
 # Plot the mean image and standard deviation image
- using StatsBase
+using StatsBase
 samples = mms.(sample(hchain, 500))
 imgs = intensitymap.(samples, fovx, fovy, 256, 256)
 
