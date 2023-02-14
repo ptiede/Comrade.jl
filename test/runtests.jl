2c4a7d23a03a284916e4678d548c9fb90c28e89f
Paul Tiede <ptiede91@gmail.com>
2023-02-13 22:52:13 -0500

    update tests to hopefully include cov for all libs

--- test/runtests.jl
+++ test/runtests.jl
@@ -34,5 +34,7 @@ else
     dev_subpkg(GROUP)
     Pkg.develop(PackageSpec(url="https://github.com/ptiede/ComradeBase.jl"))
     subpkg_path = joinpath(dirname(@__DIR__), "lib", GROUP)
-    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path))
+    run_coverage = get(ENV, "COVERAGE", "false")
+    Pkg.test(PackageSpec(name=GROUP, path=subpkg_path);
+             coverage=(run_coverage == "true"))
 end
