9973484f15b81d668d0653a2906558ba9bfe47f5
Paul Tiede <ptiede91@gmail.com>
2023-03-17 21:53:20 -0400

    io testing works locally

--- test/Core/io.jl
+++ test/Core/io.jl
@@ -1,10 +1,11 @@
-using FileIO
+using PythonCall
+
 @testset "io.jl" begin
     imc = Comrade.load(joinpath(@__DIR__, "../example_image.fits"), IntensityMap)
     ime = ehtim.image.load_image(joinpath(@__DIR__, "../example_image.fits"))
     data = load_data()
-    @test size(ime.imarr("I")) == size(imc)
-    @test flux(imc) ≈ ime.total_flux()
+    @test pyconvert(Tuple, ime.imarr("I").shape) == size(imc)
+    @test flux(imc) ≈ pyconvert(Float64, ime.total_flux())
     fov = fieldofview(imc)
     @test fov.X ≈ pyconvert(Float64, ime.fovx())
     @test fov.Y ≈ pyconvert(Float64, ime.fovx())
