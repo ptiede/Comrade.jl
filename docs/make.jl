db523df6d4244e92bc870e44180298f259115f55
Paul Tiede <ptiede91@gmail.com>
2023-04-16 19:51:10 -0400

    re add geometric modeling

--- docs/make.jl
+++ docs/make.jl
@@ -40,7 +40,9 @@ OUTDIR = joinpath(@__DIR__, "src", "examples")
 SOURCE_FILES = Glob.glob("*.jl", GENERATED)
 foreach(fn -> Literate.markdown(fn, OUTDIR, documenter=true), SOURCE_FILES)
 
-MD_FILES = [joinpath("examples", "data.md")]
+MD_FILES = [joinpath("examples", "data.md"),
+            joinpath("examples", "geometric_modeling.jl"),
+           ]
 # joinpath.("examples", replace.(basename.(SOURCE_FILES), ".jl"=>".md"))
 
 
