f0812fe62c31f1b1202c6374962294733e1403e0
Paul Tiede <ptiede91@gmail.com>
2023-06-09 12:52:01 -0400

    update to Rice for vis amps

--- src/distributions/radiolikelihood.jl
+++ src/distributions/radiolikelihood.jl
@@ -318,7 +318,7 @@ function makelikelihood(data::Comrade.EHTObservation{<:Real, <:Comrade.EHTVisibi
     Σ = data[:error].^2
     amp = data[:measurement]
     ℓ = ConditionedLikelihood(amp) do μ
-        AmplitudeLikelihood(abs.(μ), Σ)
+        RiceAmplitudeLikelihood(abs.(μ), Σ)
     end
     return ℓ
 end
