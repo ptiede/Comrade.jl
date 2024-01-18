ef535a93fa976561b40e74286c2ebde2c4a2e4d0
Paul Tiede <ptiede91@gmail.com>
2024-01-18 12:51:42 -0500

    Fix testing

--- lib/ComradeOptimization/test/runtests.jl
+++ lib/ComradeOptimization/test/runtests.jl
@@ -1,4 +1,5 @@
-using Pyehtim, Comrade, ComradeOptimization, OptimizationOptimJL, Distributions, VLBIImagePriors
+using Comrade, ComradeOptimization
+using Pyehtim, OptimizationOptimJL, Distributions, VLBIImagePriors
 using Zygote
 using Test
 
@@ -10,20 +11,20 @@ include(joinpath(@__DIR__, "../../../test/test_util.jl"))
     lklhd = RadioLikelihood(test_model, lcamp, cphase)
     post = Posterior(lklhd, prior)
 
-    tpost = ascube(post)
+    tpost = asflat(post)
     f = OptimizationFunction(tpost, Optimization.AutoZygote())
-    x0 = [ 0.1,
-           0.4,
-           0.5,
-           0.8,
-           0.3,
-           0.1,
-           0.4,
-           0.7,
-           0.8,
-           0.8]
-    prob = OptimizationProblem(f, x0, nothing, lb=fill(0.001, 10), ub=fill(0.999,10))
-    sol = solve(prob, LBFGS(); maxiters=100_000)
+    x0 = [  0.21073019358414513,
+            0.13780160840617572,
+            0.39730883437243103,
+           -0.0376931744475234,
+            0.3662436692551876,
+           -0.03851423918413366,
+           -0.28915094775302785,
+           -0.24972299832315636,
+           -0.34200263379293494,
+            0.19287666836584216,]
+    prob = OptimizationProblem(f, x0, nothing)
+    sol = solve(prob, LBFGS(); maxiters=10_000)
 
     xopt = transform(tpost, sol)
     @test isapprox(xopt.f1/xopt.f2, 2.0, atol=1e-3)
@@ -35,5 +36,6 @@ include(joinpath(@__DIR__, "../../../test/test_util.jl"))
     @test isapprox(xopt.ξ2, π/6, atol=1e-3)
     @test isapprox(xopt.x, μas2rad(30.0), rtol=1e-3)
     @test isapprox(xopt.y, μas2rad(30.0), rtol=1e-3)
+    @test chi2(skymodel(post, xopt), lcamp, cphase) < 0.1
 
 end
