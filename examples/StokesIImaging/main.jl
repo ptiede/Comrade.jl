c1e4e17141e09881c9187c35c6ce004fb5d72b9a
Paul Tiede <ptiede91@gmail.com>
2024-01-20 23:40:10 -0500

    Remove logging

--- examples/StokesIImaging/main.jl
+++ examples/StokesIImaging/main.jl
@@ -10,12 +10,11 @@
 # To get started we load Comrade.
 import Pkg #hide
 __DIR = @__DIR__ #hide
-pkg_io = open(joinpath(__DIR, "pkg.log"), "w") #hide
-Pkg.activate(__DIR; io=pkg_io) #hide
-Pkg.develop(; path=joinpath(__DIR, "..", ".."), io=pkg_io) #hide
-Pkg.instantiate(; io=pkg_io) #hide
-Pkg.precompile(; io=pkg_io) #hide
-close(pkg_io) #hide
+# pkg_io = open(joinpath(__DIR, "pkg.log"), "w") #hide
+Pkg.activate(__DIR) #hide
+Pkg.develop(; path=joinpath(__DIR, "..", "..")) #hide
+Pkg.instantiate(;) #hide
+Pkg.precompile(;) #hide
 
 using Comrade
 
