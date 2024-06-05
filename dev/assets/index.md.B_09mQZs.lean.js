55c33b50ea01f814c78ab4d2fd93327776903a64
Documenter.jl <documenter@juliadocs.github.io>
2024-06-05 19:35:53 +0000

    build based on 5395924

--- dev/assets/index.md.B_09mQZs.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as a,o,a6 as i}from"./chunks/framework.Ck1_I4DC.js";const g=JSON.parse('{"title":"Comrade","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}'),t={name:"index.md"},r=i("",12),n=[r];function s(l,d,m,u,h,c){return o(),a("div",null,n)}const p=e(t,[["render",s]]);export{g as __pageData,p as default};
