dac9ec9417bb94e3d68370d35a158ac8172f8394
Documenter.jl <documenter@juliadocs.github.io>
2024-07-01 18:37:26 +0000

    build based on 34715d9

--- dev/assets/api.md.b7WuEBk5.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as i,c as s,o as a,a6 as e}from"./chunks/framework.DXK9xsT5.js";const b=JSON.parse('{"title":"Comrade API","description":"","frontmatter":{},"headers":[],"relativePath":"api.md","filePath":"api.md","lastUpdated":null}'),t={name:"api.md"},r=e("",232),l=[r];function n(d,h,p,o,k,c){return a(),s("div",null,l)}const u=i(t,[["render",n]]);export{b as __pageData,u as default};
