139a75456a349723c5d4a7010fa7a460f6372b81
Paul Tiede <ptiede91@gmail.com>
2025-03-30 13:53:32 -0400

    Update versions

--- ext/ComradePigeonsExt.jl
+++ ext/ComradePigeonsExt.jl
@@ -4,6 +4,7 @@ using Comrade
 
 using Pigeons
 using AbstractMCMC
+using EnzymeCore
 using LogDensityProblems
 using HypercubeTransform
 using TransformVariables
@@ -70,5 +71,17 @@ end
 LogDensityProblems.dimension(t::PriorRef) = Comrade.dimension(t.transform)
 LogDensityProblems.logdensity(t::PriorRef, x) = t(x)
 LogDensityProblems.capabilities(::Type{<:PriorRef}) = LogDensityProblems.LogDensityOrder{0}()
+
+function LogDensityProblems.logdensity_and_gradient(log_potential::Pigeons.BufferedAD{<:Comrade.AbstractVLBIPosterior}, x)
+    m = log_potential.enclosed
+    b = log_potential.buffer
+    ∂ℓ_∂x = fill!(b, zero(eltype(b.buffer))) # NB: Enzyme gives erroneous answer if buffer is not zeroed first
+    mode = EnzymeCore.WithPrimal(Comrade.admode(d))
+    _, y = EnzymeCore.autodiff(
+                mode, LogDensityProblems.logdensity, EnzymeCore.Active,
+                EnzymeCore.Const(m), EnzymeCore.Duplicated(x, ∂ℓ_∂x)
+            )
+    return y, ∂ℓ_∂x
+end
 
 end
