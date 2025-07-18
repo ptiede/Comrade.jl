bef9ce315290b359f1d63e77b33642de18c956b0
Paul Tiede <ptiede91@gmail.com>
2025-07-18 11:14:50 -0400

    Start for a better data structure

--- src/observations/abstract.jl
+++ src/observations/abstract.jl
@@ -49,3 +49,44 @@ Base.getindex(data::AbstractVLBITable, i::Int) = build_datum(data, i)
 function VLBISkyModels.rebuild(config::AbstractVLBITable, table)
     throw(MethodError(rebuild, config, table))
 end
+
+
+abstract type AbstractMount end
+
+"""
+    Mount(parallactic, elevation, offset=0)
+
+Defines the telescope mount type. The parallactic argument controls the evolution
+of the parallactic angle, the elevation controls the elevation of the mount, 
+and the offset is an optional offset of the feeds in radians.
+
+For specific telescopes you can use the convenience constructors:
+- `MountCassegrain(offset=0)`: for Cassegrain mounts
+- `MountNasmythR(offset=0)`: for right Nasmyth mounts
+- `MountNasymthL(offset=0)`: for left Nasmyth mounts
+"""
+struct Mount{A, B, C} <: AbstractMount
+    parallactic::A
+    elevation::B
+    offset::C
+    function Mount(parallactic::A, elevation::B, offset::C = 0.0) where {A, B, C}
+        return new{A, B, C}(parallactic, elevation, offset)
+    end
+end
+
+parallactic_mount(m::Mount) = getfield(m, :parallactic)
+elevation_mount(m::Mount) = getfield(m, :elevation)
+offset_mount(m::Mount) = getfield(m, :offset)
+
+MountCassegrain(offset = 0) = Mount(1, 0, offset)
+MountNasmythR(offset = 0) = Mount(1, 1, offset)
+MountNasymthL(offset = 0) = Mount(1, -1, offset)
+
+
+Base.@kwdef struct Antenna{N, T, M <: AbstractMount, F <: PolBasis}
+    name::N
+    position::SVector{3, T}
+    mount::M
+    feed::F
+end
+
